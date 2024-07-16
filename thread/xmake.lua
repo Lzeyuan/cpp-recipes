e96fdfc0acc6a4abd7360d1b7d5f58f999effacd
Your leza <1046527915@qq.com>
2024-07-16 10:15:39 +0000

    add gtest

--- thread/xmake.lua
+++
@@ -1,7 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-target("thread")
-    set_languages("c++20")
-    set_kind("binary")
-    add_files("test/*.cpp")
-    add_includedirs("$(curdir)/thread")
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,19 @@
-set_project("LearnOpenGL")
+set_project("recipes")
 set_version("1.0.0")
 set_languages("c++17")
-
-target("base")
-    set_kind("static")
     
-includes("*/xmake.lua")
+includes("src/*/xmake.lua")
+
+-- Define the test dependency
+add_requires("gtest", {configs = {main = false, gmock = true}})
+-- Iterate over test files and create test targets
+for _, file in ipairs(os.files("tests/*Test.cpp")) do
+    local name = path.basename(file)
+    target(name)
+        add_deps("thread")
+        set_kind("binary")
+        add_packages("gtest")
+        set_default(false)
+        add_files(file)
+        add_tests("default")
+end
